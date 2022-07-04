e3b6b2649e69a86198352f9509dfc017d06edfd0
fullDev9233 <57453757+fullDev9233@users.noreply.github.com>
2022-07-04 13:05:20 -0400

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
 import { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Header from './components/Header';
+import { useAppSelector } from './hooks/useRedux';
 import PrivateRoute from './routers/PrivateRoute';
+import { selectIsLoggedIn } from './redux/authSlice';
 import GlobalStyle from './styles/global';
 
 const Home = lazy(() => import('./pages/Home'));
 const Trade = lazy(() => import('./pages/Trade'));
 
-const App = () => (
-  <Router>
-    <GlobalStyle />
-    <Header />
-    <Suspense fallback={<div>Loading...</div>}>
-      <Switch>
-        <PrivateRoute exact path="/">
-          <Home />
-        </PrivateRoute>
-        <PrivateRoute exact path="/trade">
-          <Trade />
-        </PrivateRoute>
-      </Switch>
-    </Suspense>
-  </Router>
-);
+const App = () => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+
+  return (
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/sign-in">
+            {isLoggedIn && <Redirect to="/" />}
+          </Route>
+          <PrivateRoute exact path="/">
+            <Home />
+          </PrivateRoute>
+          <PrivateRoute exact path="/trade">
+            <Trade />
+          </PrivateRoute>
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
 
 export default App;
